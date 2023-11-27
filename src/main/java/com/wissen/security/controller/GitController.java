9e1c4c6fc9f8eaf063c2fc7e81df6172095d5f49
ywissen <burada.yogeshwararao@wissen.com>
2023-11-27 16:33:24 +0530

    Adding changes from dev-1

--- src/main/java/com/wissen/security/controller/GitController.java
+++ src/main/java/com/wissen/security/controller/GitController.java
@@ -13,7 +13,7 @@ public class GitController {
 	
 	@GetMapping("/order/")
 	public String getOrderDetails(@RequestBody Order order) {
-		System.out.println("i am modifiying by dev-3");
+		System.out.println("i am modifiying by dev-1");
 		return order.getOname()+" "+order.getOid()+" "+order.getCost();
 	}
 
