80201479105d1de91c25eb8f93da9fbc81fddd39
ywissen <burada.yogeshwararao@wissen.com>
2023-11-27 14:08:51 +0530

    added changes to dev-1 repo

--- src/main/java/com/wissen/security/controller/GitController.java
+++ src/main/java/com/wissen/security/controller/GitController.java
@@ -1,17 +1,19 @@
 package com.wissen.security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wissen.security.model.Order;
 
 @RestController
 @RequestMapping("/api")
 public class GitController {
 	
-	@GetMapping("/order/{oid}/{oname}/{cost}")
-	public String getOrderDetails(@PathVariable Integer oid,@PathVariable String oname,@PathVariable Long cost) {
-		return oid+" "+oname+" "+cost;
+	@GetMapping("/order/")
+	public String getOrderDetails(@RequestBody Order order) {
+		return order.getOname()+" "+order.getOid()+" "+order.getCost();
 	}
 
 }
