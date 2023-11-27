6a6c4fc48688dcd136fdd672e5141ee845368a89
ywissen <burada.yogeshwararao@wissen.com>
2023-11-27 16:29:03 +0530

    Adding changes from dev-3

--- src/main/java/com/wissen/security/controller/GitController.java
+++ src/main/java/com/wissen/security/controller/GitController.java
@@ -13,6 +13,7 @@ public class GitController {
 	
 	@GetMapping("/order/")
 	public String getOrderDetails(@RequestBody Order order) {
+		System.out.println("i am modifiying by dev-3");
 		return order.getOname()+" "+order.getOid()+" "+order.getCost();
 	}
 
