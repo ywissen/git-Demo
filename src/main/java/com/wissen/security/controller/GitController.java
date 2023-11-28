b3ec4a733c848a2320406ba6b46d2b09ca82ed37
ywissen <burada.yogeshwararao@wissen.com>
2023-11-28 13:41:44 +0530

    dev1-Master-Commit

--- src/main/java/com/wissen/security/controller/GitController.java
+++ src/main/java/com/wissen/security/controller/GitController.java
@@ -13,7 +13,7 @@ public class GitController {
 	
 	@GetMapping("/order/")
 	public String getOrderDetails(@RequestBody Order order) {
-		System.out.println("i am modifiying by dev-1");
+		System.out.println("I am devbeloper 1 first Dev");
 		return order.getOname()+" "+order.getOid()+" "+order.getCost();
 	}
 
